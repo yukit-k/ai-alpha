007c8a53a3e7ce545a8540f845bac97d26f74362
yuki678 <uk.12juni@gmail.com>
2020-12-10 22:30:48 +0000

    Added Trailing Stop to the Lean Tutorial

--- LeanTutorial.py
+++ LeanTutorial.py
@@ -36,3 +36,51 @@ class FXBuyHold(QCAlgorithm):
         #3. Using "Portfolio.Invested" submit 1 order for 2000 AUDUSD:
         if not self.Portfolio.Invested:
             self.MarketOrder("AUDUSD", 2000)
+
+# Buy and Hold with a Trailing Stop
+
+class BootCampTask(QCAlgorithm):
+    
+    # Order ticket for our stop order, Datetime when stop order was last hit
+    stopMarketTicket = None
+    stopMarketOrderFillTime = datetime.min
+    highestSPYPrice = -1
+    
+    def Initialize(self):
+        self.SetStartDate(2018, 12, 1)
+        self.SetEndDate(2018, 12, 10)
+        self.SetCash(100000)
+        spy = self.AddEquity("SPY", Resolution.Daily)
+        spy.SetDataNormalizationMode(DataNormalizationMode.Raw)
+        
+    def OnData(self, data):
+        
+        # 1. Plot the current SPY price to "Data Chart" on series "Asset Price"
+        self.Plot("Data Chart", "Asset Price", data["SPY"].Close)
+
+        if (self.Time - self.stopMarketOrderFillTime).days < 15:
+            return
+
+        if not self.Portfolio.Invested:
+            self.MarketOrder("SPY", 500)
+            self.stopMarketTicket = self.StopMarketOrder("SPY", -500, 0.9 * self.Securities["SPY"].Close)
+        
+        else:
+            
+            #2. Plot the moving stop price on "Data Chart" with "Stop Price" series name
+            self.Plot("Data Chart", "Stop Price", self.highestSPYPrice * 0.9)
+            
+            if self.Securities["SPY"].Close > self.highestSPYPrice:
+                
+                self.highestSPYPrice = self.Securities["SPY"].Close
+                updateFields = UpdateOrderFields()
+                updateFields.StopPrice = self.highestSPYPrice * 0.9
+                self.stopMarketTicket.Update(updateFields) 
+            
+    def OnOrderEvent(self, orderEvent):
+        
+        if orderEvent.Status != OrderStatus.Filled:
+            return
+        
+        if self.stopMarketTicket is not None and self.stopMarketTicket.OrderId == orderEvent.OrderId: 
+            self.stopMarketOrderFillTime = self.Time
